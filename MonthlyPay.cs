fd831b8dc28ade33a99e0ac604dce2407f975fd4
SakshiBobade05 <88444417+SakshiBobade05@users.noreply.github.com>
2021-08-18 16:36:32 +0530

    first

---
+++ MonthlyPay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class MonthlyPay
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Monthly Payment:");
+            Double m = double.Parse(Console.ReadLine());
+            int month = 0;
+            double loan = 1000;
+            while (loan > m)
+            {
+                loan *= 1.015;
+                loan -= m;
+                Console.WriteLine("Balance: " + loan + "  Month: " + (++month) + " Total Payments:" + (m * month));
+            }
+        }
+    }
+}
